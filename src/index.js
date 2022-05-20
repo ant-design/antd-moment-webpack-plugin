2e837786e882fdee1387ce5e565d78913e4bfe1c
dong-xn <84005146+dong-xn@users.noreply.github.com>
2022-05-20 21:56:25 +0800

    feat: create momentPlugin (#1)
    Co-authored-by: 董鑫楠 <dongxinnan@dongxinnandeMacBook-Pro.local>

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class momentPlugin {
+    apply(compiler) {
+      const { alias } = compiler.options.resolve;
+      if (alias) {
+        alias.dayjs = 'moment';
+      } else {
+        compiler.options.resolve.alias = {
+          dayjs: 'moment'
+        };
+      }
+    }
+  }
+  
+  module.exports = momentPlugin;
