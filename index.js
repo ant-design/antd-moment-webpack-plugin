2b5ba978390cebc669526aa0427cfe8d555b9b6a
iamkun <kunhello@outlook.com>
2022-06-07 19:12:22 +0800

    Create index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const generateRegExp = /generate\/dayjs/;
+const updateResource = (resource) => {
+  resource.request = resource.request.replace('dayjs', 'moment');
+};
+const plugin = 'AntdMomentWebpackPlugin';
 class Plugin {
   apply(compiler) {
     const { alias } = compiler.options.resolve;
@@ -5,10 +10,22 @@ class Plugin {
       alias.dayjs = 'moment';
     } else {
       compiler.options.resolve.alias = {
-        dayjs: 'moment'
+        dayjs: 'moment',
       };
     }
+    compiler.hooks.normalModuleFactory.tap(plugin, (factory) => {
+      factory.hooks.beforeResolve.tap(plugin, (result) => {
+        if (generateRegExp.test(result.request)) {
+          updateResource(result);
+        }
+      });
+      factory.hooks.afterResolve.tap(plugin, (result) => {
+        const createData = result.createData;
+        if (generateRegExp.test(createData.resource)) {
+          updateResource(result);
+        }
+      });
+    });
   }
-}
-
+};
 module.exports = Plugin;
